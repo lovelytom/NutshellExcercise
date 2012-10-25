bede22bb8d1b4ec95cba0ba46534433901843606
Lovely <lovely7paul@gmail.com>
2012-10-25 20:08:51 +0530

    Removed default namespaces

--- NutshellExcercise/Program.cs
+++ NutshellExcercise/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace NutshellExcercise
 {
@@ -10,6 +6,7 @@ namespace NutshellExcercise
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
